a3cdee7e65c303b09a8d18650cccfbd24de090a8
Alex Polvi <alex@polvi.net>
2013-04-15 12:05:35 -0700

    add dist stuff

---
+++ dist/config.py
@@ -0,0 +1,5 @@
+config = {
+  'host_pass': 'Y0UR-DEFAULT', # password for fabric
+  'api_user': 'rax-api-user',  # rackspace cloud api user
+  'api_key': 'rax-api-key',    # rackspac cloud api key
+}
