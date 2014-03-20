49bd2d84114cd991a6bf476a5a1c9f1b1ab8f4d5
Alex Polvi <alex@polvi.net>
2014-03-20 11:52:24 -0700

    just a few random tweaks

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,8 @@ def ssh(name):
   execute(open_shell)
 
 def setup_host():
-  run('apt-get -y install parted bzip2')
+  run('apt-get update')
+  run('apt-get -y install curl bzip2')
 
 def fetch_image_dd(image_loc):
   run('curl %s | bunzip2 | dd of=/dev/xvdb bs=128M' % (image_loc))
@@ -22,18 +23,20 @@ def run_all(image_name, image_loc):
   node = create_node(name)
   rescue_node(name)
 
+#  ssh(name)
   # fabric stuff
   _set_hosts_by_node(node)
   execute(setup_host)
   execute(fetch_image_dd, image_loc)
 
   unrescue_node(name)
-#  new_node = save_image(name, image_name)
+  new_node = save_and_create(name, image_name)
 
   # will try to ssh to the machine and run uname
   # will only work if the default password is falkor
-#  _set_hosts_by_node(new_node)
-#  execute(uname)
+  _set_hosts_by_node(new_node)
+  env.user = 'core'
+  execute(uname)
 
 
 def save_and_create(base_name, image_name):
@@ -41,6 +44,7 @@ def save_and_create(base_name, image_name):
   while True:
     try:
       new_node = create_node(image_name, image)
+      print "created new node with image...", new_node
       return new_node
     except Exception:
       print "waiting on image to be ready..."
@@ -99,10 +103,12 @@ def create_node(name, image=None):
     return nodes[0]
 
   sizes = driver.list_sizes()
-  size = [s for s in sizes if s.ram == 512][0]
+  #size = [s for s in sizes if s.id == 'performance1-1'][0]
+  size = [s for s in sizes if s.id == 'performance1-2'][0]
   if image is None:
     images = driver.list_images() 
-    image = [i for i in images if i.name == 'Debian 6.06 (Squeeze)'][0]
+    image = [i for i in images if i.name == 'Debian 7 (Wheezy) (PVHVM)'][0]
+#    image = [i for i in images if i.name == 'Debian 6.06 (Squeeze)'][0]
   node = driver.create_node(name=name, size=size, image=image)
   nodes = driver.wait_until_running(nodes=[node])
   return nodes[0][0]
@@ -126,6 +132,8 @@ def rescue_node(name):
   while node.state != NodeState.PENDING:
     node = [x for x in driver.list_nodes() if x.name == name][0]
     time.sleep(3)
+  # wait for ssh to come up
+  time.sleep(10)
 
 def unrescue_node(name):
   driver = _get_rack_driver()
